2342bf4c6690f75b7a5ec819ab59ce21abd5982a
nikita1803 <rainikita595@gmail.com>
2021-05-21 21:19:57 +0530

    Added: First name validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,9 +1,28 @@
 package userregistration;
 
-public class UserRegistration {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration 
+{
+	public static boolean usernameValidation(String name) 
+	{
+		String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		if (name == null) 
+		{
+			return false;
+		}
+		 Matcher m = p.matcher(name);
+		 return m.matches();	 
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome in User Registration");
-
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Enter the your First Name : ");
+		String fname = input.nextLine();	 
+	    System.out.println(usernameValidation(fname));
 	}
 }
